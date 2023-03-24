fa3910cc78d55c43e1760fbc4d30cedfef7ba74a
PatignierThomas <thomas.patignier@gmail.com>
2023-03-24 18:13:39 +0100

    Initial commit

--- src/Portfolio/settings.py
+++ src/Portfolio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vps-525b741b.vps.ovh.net']
 
 
 # Application definition
@@ -132,7 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = False
-EMAIL_PORT = 2500
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
 # EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 # EMAIL_HOST_PASSWORD=env(EMAIL_HOST_PASSWORD)
